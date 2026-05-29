39d42b7adfe15ef3f862ce1b405c32c751336d00
yeironflorian9-cmd <yeironflorian9@gmail.com>
2026-05-29 13:58:22 -0500

    Add files via upload

---
+++ Programa 2.16/Untitled1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* Empresa textil.
+El programa, al recibir como datos decisivos la categoría y antiguedad de
+un empleado, determina si el mismo reúne las condiciones establecidas por
+la empresa para ocupar un nuevo cargo en una sucursal.
+
+CLA, CAT, ANT, RES: Variables de tipo entero.
+SAl: Variable de tipo real. */
+
+void main(void)
+{
+    int CLA,CAT, ANT, RES;
+    printf("\nIngrese la clave, categoría y antiguedad del trabajador: ");
+    scanf("%d %d %d", &CLA, &CAT, &ANT);
+    switch (CAT)
+    {
+    case 3:
+    case 4: if (ANT >= 5)
+                RES = 1;
+                else
+                    RES = 0;
+                break;
+    case 2: if (ANT >= 7)
+                     RES = 1;
+            else
+                RES = 0;
+            break;
+            default: RES = 0;
+            break;
+    }
+    if (RES)
+    printf("\nEl trabajador con clave %d reúne las condiciones para el puesto", CLA);
+    else
+    printf("\nEl trabajador con clave %d no reúne las condiciones para el puesto", CLA);
+}
