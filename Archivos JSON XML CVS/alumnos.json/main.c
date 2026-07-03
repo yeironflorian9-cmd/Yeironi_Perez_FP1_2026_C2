d6154a98dab7e923cfa91e1eed32a60f988b2cd0
yeironflorian9-cmd <yeironflorian9@gmail.com>
2026-07-03 09:09:41 -0500

    Add files via upload

---
+++ Archivos JSON XML CVS/alumnos.json/main.c
@@ -0,0 +1,64 @@
+{
+  "alumnos": [
+    {
+      "id":1,
+      "nombre":"Juan Pérez",
+      "edad":20,
+      "carrera":"Informática"
+    },
+    {
+      "id":2,
+      "nombre":"Ana Gómez",
+      "edad":19,
+      "carrera":"Software"
+    },
+    {
+      "id":3,
+      "nombre":"Carlos Ruiz",
+      "edad":21,
+      "carrera":"Redes"
+    },
+    {
+      "id":4,
+      "nombre":"Laura Díaz",
+      "edad":20,
+      "carrera":"Multimedia"
+    },
+    {
+      "id":5,
+      "nombre":"Pedro Santos",
+      "edad":22,
+      "carrera":"Informática"
+    },
+    {
+      "id":6,
+      "nombre":"María López",
+      "edad":18,
+      "carrera":"Software"
+    },
+    {
+      "id":7,
+      "nombre":"José Martínez",
+      "edad":19,
+      "carrera":"Redes"
+    },
+    {
+      "id":8,
+      "nombre":"Luisa Fernández",
+      "edad":20,
+      "carrera":"Ciberseguridad"
+    },
+    {
+      "id":9,
+      "nombre":"Miguel Torres",
+      "edad":23,
+      "carrera":"Software"
+    },
+    {
+      "id":10,
+      "nombre":"Andrea Castillo",
+      "edad":21,
+      "carrera":"Informática"
+    }
+  ]
+}
---
+++ Archivos JSON XML CVS/alumnos.xml/main.c
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<alumnos>
+
+    <alumno>
+        <id>1</id>
+        <nombre>Juan Pérez</nombre>
+        <edad>20</edad>
+        <carrera>Informática</carrera>
+    </alumno>
+
+    <alumno>
+        <id>2</id>
+        <nombre>Ana Gómez</nombre>
+        <edad>19</edad>
+        <carrera>Software</carrera>
+    </alumno>
+
+    <alumno>
+        <id>3</id>
+        <nombre>Carlos Ruiz</nombre>
+        <edad>21</edad>
+        <carrera>Redes</carrera>
+    </alumno>
+
+    <alumno>
+        <id>4</id>
+        <nombre>Laura Díaz</nombre>
+        <edad>20</edad>
+        <carrera>Multimedia</carrera>
+    </alumno>
+
+    <alumno>
+        <id>5</id>
+        <nombre>Pedro Santos</nombre>
+        <edad>22</edad>
+        <carrera>Informática</carrera>
+    </alumno>
+
+    <alumno>
+        <id>6</id>
+        <nombre>María López</nombre>
+        <edad>18</edad>
+        <carrera>Software</carrera>
+    </alumno>
+
+    <alumno>
+        <id>7</id>
+        <nombre>José Martínez</nombre>
+        <edad>19</edad>
+        <carrera>Redes</carrera>
+    </alumno>
+
+    <alumno>
+        <id>8</id>
+        <nombre>Luisa Fernández</nombre>
+        <edad>20</edad>
+        <carrera>Ciberseguridad</carrera>
+    </alumno>
+
+    <alumno>
+        <id>9</id>
+        <nombre>Miguel Torres</nombre>
+        <edad>23</edad>
+        <carrera>Software</carrera>
+    </alumno>
+
+    <alumno>
+        <id>10</id>
+        <nombre>Andrea Castillo</nombre>
+        <edad>21</edad>
+        <carrera>Informática</carrera>
+    </alumno>
+
+</alumnos>
